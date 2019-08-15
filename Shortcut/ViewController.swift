f01966a6de1fecb47806d1d626d63065aa1470d0
Mohamed Kotb <mo.kotb96@gmail.com>
2019-08-15 23:33:48 +0200

    Initial Commit

---
+++ Shortcut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shortcut
+//
+//  Created by Mohamed Kotb on 8/15/19.
+//  Copyright © 2019 Mohamed Kotb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
